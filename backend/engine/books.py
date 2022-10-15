1fe509872b65eaf44c2dffa6dbaef0bfd406ce40
tamuto <mutomob@gmail.com>
2022-10-15 06:06:32 +0000

    インサート対応

--- backend/engine/books.py
+++ backend/engine/books.py
@@ -4,10 +4,13 @@ from glob import glob
 from ..utils import config
 
 
-def make_item(filename):
+def load_yaml(filename):
     with open(filename, 'r', encoding='utf-8') as f:
-        obj = yaml.safe_load(f)
+        return yaml.safe_load(f)
 
+
+def make_item(filename):
+    obj = load_yaml(filename)
     return {
         'book_id': obj['book_id'],
         'book_name': obj['book_name']
@@ -17,3 +20,8 @@ def make_item(filename):
 def get_booklist():
     folder = config.get_BOOKS()
     return [make_item(f) for f in glob(f'{folder}/*.yaml')]
+
+
+def get_book(book_id):
+    folder = config.get_BOOKS()
+    return load_yaml(f'{folder}/{book_id}.yaml')
---
+++ backend/engine/records.py
@@ -0,0 +1,89 @@
+import os
+import sqlite3
+from datetime import datetime
+from dbcolls.sqlite3.Row import DictRow
+
+from ..utils import config
+
+sql_create_st_book_record = '''
+CREATE TABLE st_book_record (
+    book_id VARCHAR(36) NOT NULL,
+    record_no INTEGER NOT NULL,
+    active_flag CHAR(1) NOT NULL,
+    create_date TIMESTAMP NOT NULL,
+    update_date TIMESTAMP NOT NULL,
+    PRIMARY KEY (book_id, record_no)
+)
+'''
+
+sql_create_st_book_field = '''
+CREATE TABLE st_book_field (
+    book_id VARCHAR(36) NOT NULL,
+    record_no INTEGER NOT NULL,
+    field_name VARCHAR(128) NOT NULL,
+    value VARCHAR(512),
+    PRIMARY KEY (book_id, record_no, field_name)
+)
+'''
+
+sql_select_record = "SELECT record_no FROM st_book_record WHERE active_flag = 'Y' ORDER BY record_no"
+sql_select_field = "SELECT field_name, value FROM st_book_field WHERE record_no = :record_no"
+
+sql_select_max = "SELECT COUNT(record_no) as last_no FROM st_book_record"
+sql_insert_record = "INSERT INTO st_book_record VALUES (:book_id, :record_no, :active_flag, :create_date, :update_date)"
+sql_insert_field = "INSERT INTO st_book_field VALUES (:book_id, :record_no, :field_name, :value)"
+
+
+class DataManager:
+
+    def __init__(self, book_id):
+        folder = config.get_BOOK_DATA()
+        self.book_id = book_id
+        self.database = f'{folder}/{self.book_id}.sqlite'
+        new_flag = True
+        if os.path.exists(self.database):
+            new_flag = False
+        self.conn = sqlite3.connect(self.database)
+        self.conn.row_factory = DictRow
+        if new_flag is True:
+            self.conn.execute(sql_create_st_book_record)
+            self.conn.execute(sql_create_st_book_field)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, ex_type, ex_value, trace):
+        self.conn.close()
+
+    def query_all(self):
+        result = self.conn.execute(sql_select_record)
+        return [self.query(r.record_no) for r in result]
+
+    def query(self, record_no):
+        result = self.conn.execute(sql_select_field, {'record_no': record_no})
+        return {r.field_name: r.value for r in result}
+
+    def insert(self, data):
+        max_record_no = self.conn.execute(sql_select_max).fetchone().last_no + 1
+        self.conn.execute(sql_insert_record, {
+            'book_id': self.book_id,
+            'record_no': max_record_no,
+            'active_flag': 'Y',
+            'create_date': datetime.now(),
+            'update_date': datetime.now()
+        })
+
+        for field, value in data.items():
+            self.conn.execute(sql_insert_field, {
+                'book_id': self.book_id,
+                'record_no': max_record_no,
+                'field_name': field,
+                'value': value
+            })
+        self.conn.commit()
+
+    def update(self, record_no, data):
+        pass
+
+    def delete(self, record_no):
+        pass
