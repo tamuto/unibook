7a86597a8d46a5696b3e8ddbf7eb36ca5259eb8d
nana.ishimatsu <nana.ishimatsu@abi-inc.co.jp>
2023-03-15 02:28:35 +0000

    Stackタグ修正

--- frontend/features/bookform/components/BookForm.js
+++ frontend/features/bookform/components/BookForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import styled from '@emotion/styled'
 import {
   Button,
   IconButton,
@@ -12,15 +13,32 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import OutputIcon from '@mui/icons-material/Output'
 
+import Display from './Display'
 import HeadBox from '../../../conponents/Header'
 import RequestConfirmation from '~/conponents/RequestConfirmation'
 import Search from './Search'
-import Display from './Display'
 import useBookForm from '../api/useBookForm'
 
+import DataCell from 'github://tamuto/boilerplate/uilib/components/datarow/DataCell.js'
+import DataHead from 'github://tamuto/boilerplate/uilib/components/datarow/DataHead.js'
+import DataRow from 'github://tamuto/boilerplate/uilib/components/datarow/DataRow.js'
+
+const DataTable = styled.div`
+  & .DataHead {
+    border-bottom: solid 2px grey;
+    background-color: #a0d0d0;
+  }
+  & .DataRow {
+    border-bottom: solid 1px grey;
+  }
+  & .DataRow:nth-of-type(odd) {
+    background-color: #FFFFFF;
+  }
+`
 
 const BookForm = () => {
   const { openModal, data, newEntry, moveHome, moveEdit, toggleModal, moveDelete } = useBookForm()
+
   return (
     <>
       <Stack spacing={2}>
@@ -55,44 +73,43 @@ const BookForm = () => {
           <Search />
           <Display />
         </Stack>
-        <Stack spacing={2} direction='row' sx={{ backgroundColor: 'white', p: 2 }}>
-          {
-            data.columns.map((item, idx) => (
-              <Stack sx={{ width: '40%' }} key={idx}>
-                {item.caption}
-              </Stack>
-            ))
-          }
-          <Stack sx={{ width: '20%' }}></Stack>
-          <Stack sx={{ width: '20%' }}></Stack>
-        </Stack>
-        <Stack>
+        <DataTable>
+          <DataHead>
+            {
+              data.columns.map((item, idx) => (
+                <DataCell key={idx} style={{ width: `${item.size}px` }}>
+                  {item.caption}
+                </DataCell>
+              ))
+            }
+            <DataCell />
+          </DataHead>
           {
             data.records.map((record, idx) => (
-              <Stack key={idx} direction='row' spacing={2} sx={{ borderBottom: 1, px: 2 }}>
+              <DataRow key={idx}>
                 {
                   data.columns.map((item) => (
-                    <Stack sx={{ width: '40%' }} key={item.field_name}>{record[item.field_name]}</Stack>
+                    <DataCell key={item.field_name} style={{ width: `${item.size}px` }}>
+                      {record[item.field_name]}
+                    </DataCell>
                   ))
                 }
-                <Stack sx={{ width: '20%' }}>
+                <DataCell>
                   <Tooltip title='編集'>
-                    <IconButton sx={{ cursor: 'pointer' }} onClick={() => moveEdit(record)} >
+                    <IconButton onClick={() => moveEdit(record)} >
                       <EditIcon />
                     </IconButton>
                   </Tooltip>
-                </Stack>
-                <Stack sx={{ width: '20%' }}>
                   <Tooltip title='削除'>
-                    <IconButton color='error' sx={{ cursor: 'pointer' }} onClick={() => toggleModal(record)} >
+                    <IconButton color='error' onClick={() => toggleModal(record)} >
                       <DeleteIcon />
                     </IconButton>
                   </Tooltip>
-                </Stack>
-              </Stack>
+                </DataCell>
+              </DataRow>
             ))
           }
-        </Stack>
+        </DataTable>
         <RequestConfirmation
           open={openModal}
           description={<>
